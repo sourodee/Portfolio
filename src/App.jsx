76e6ea237c58965a26c8d6d83a911a0e687ee50b
Akash Mukherjee <akash3.11.2000@gmail.com>
2023-07-21 13:48:07 +0530

    lazy load promise removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Suspense, useContext } from "react";
-import lazyLoad from "./Util/lazyLoad";
+import { Suspense, lazy, useContext } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Loader from "./Components/Loader";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import { ThemeContext } from "./Contexts/themeContext";
-const Work = lazyLoad(import("./Pages/Work"));
-const Home = lazyLoad(import("./Pages/Home"));
-const Tools = lazyLoad(import("./Pages/Tools"));
-const Experience = lazyLoad(import("./Pages/Experience"));
-const Contact = lazyLoad(import("./Pages/Contact"));
+// import lazyLoad from "./Util/lazyLoad";
+// const Work = lazyLoad(import("./Pages/Work"));
+const Work = lazy(() => import("./Pages/Work"));
+const Home = lazy(() => import("./Pages/Home"));
+const Tools = lazy(() => import("./Pages/Tools"));
+const Experience = lazy(() => import("./Pages/Experience"));
+const Contact = lazy(() => import("./Pages/Contact"));
 
 function App() {
     const location = useLocation();
@@ -22,9 +23,9 @@ function App() {
             style={
                 location.pathname === "/tools" && theme === "dark"
                     ? {
-                        backgroundImage:
-                            "url('https://media2.giphy.com/media/NzSUEgbTWB7TW/giphy.gif?cid=ecf05e47i28nmo22l74vnnkvhfzbc5zta904vifpcw0qfl9y&ep=v1_gifs_related&rid=giphy.gif&ct=s')",
-                    }
+                          backgroundImage:
+                              "url('https://media2.giphy.com/media/NzSUEgbTWB7TW/giphy.gif?cid=ecf05e47i28nmo22l74vnnkvhfzbc5zta904vifpcw0qfl9y&ep=v1_gifs_related&rid=giphy.gif&ct=s')",
+                      }
                     : {}
             }
         >
