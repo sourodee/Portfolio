b1780beff41d949d4b79f9a0460d60ed3f0c550c
Akash Mukherjee <akash3.11.2000@gmail.com>
2023-07-21 14:16:43 +0530

    vite config modified

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { dependencies } from "./package.json";
+
+function renderChunks(deps) {
+    let chunks = {};
+    Object.keys(deps).forEach((key) => {
+        if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+        chunks[key] = [key];
+    });
+    return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +17,15 @@ export default defineConfig({
         port: 3000,
     },
     plugins: [react()],
+    build: {
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ["react", "react-router-dom", "react-dom"],
+                    ...renderChunks(dependencies),
+                },
+            },
+        },
+    },
 });
